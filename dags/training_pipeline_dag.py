63dd9782117b2e21de3b39fe8b1b8f6960d1871b
Leticia <leticiamalato3@gmail.com>
2026-09-01 10:32:22 -0300

    chore: use past start_date in Airflow DAG (convention)

--- dags/training_pipeline_dag.py
+++ dags/training_pipeline_dag.py
@@ -13,7 +13,7 @@ from airflow.operators.bash import BashOperator
 with DAG(
     dag_id="triage_model_training",
     schedule=None,
-    start_date=datetime(2026, 1, 1),
+    start_date=datetime(2024, 1, 1),
     catchup=False,
 ) as dag:
     prepare_data_task = BashOperator(
